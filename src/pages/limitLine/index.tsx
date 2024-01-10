dc5b559bc69fc8c50b4bbced723d3fa3e23b9943
ForeverYoung5 <3105150874@qq.com>
2024-01-10 22:23:13 +0800

    fix:兼容不同高度的元素，同一行top相同，bottom不一定

--- src/pages/limitLine/index.tsx
+++ src/pages/limitLine/index.tsx
@@ -12,11 +12,11 @@ const questionLayoutProps: IQuestionLayoutProps = {
 };
 
 const mockElements: string[] = [];
-for (let i = 0; i < 13; i++) {
+for (let i = 0; i < 22; i++) {
   mockElements.push(`第${i}个元素`);
 };
 
-const elementMap: Map<number, { rect: any, index: number }> = new Map()
+const elementMap: Map<number, { rect: any, index: number, lineHeight: number }> = new Map()
 
 const LimitLine = () => {
   const elementContainerRef: any = useRef(null);
@@ -34,10 +34,12 @@ const LimitLine = () => {
    */
   const hideMoreElement = (line: number) => {
     const elementMapArr = Array.from(elementMap);
-    const firstLineBottom = elementMapArr[0][0]; // 获取第1行的bottom
-    const LineBottom = elementMapArr[1][0]; // 获取第2行的bottom
-    const oneLineHeight = LineBottom - firstLineBottom;
-    setElementContainerHeight(oneLineHeight * line + 'px');
+    let height = 0;
+    for (let i = 0; i < line; i++) {
+      const item = elementMapArr[i][1];
+      height += item.lineHeight;
+    };
+    setElementContainerHeight(height + 'px');
   };
 
 
@@ -55,19 +57,16 @@ const LimitLine = () => {
     } else {
       // 兼容元素总数不超过四行的情况：展示全部内容
       setElementContainerHeight('initial');
-
       // 判断最后一个元素是否占完了最后一行，占完了就替换为展开按钮，还有空间的话在最后添加展开按钮
       const beforeLastLineElement = elementMapArr[elementMapArr.length - 2][1];//倒数第二行的最后一个元素
       const lastLineElement = elementMapArr[elementMapArr.length - 1][1];//倒数第一行的最后一个元素
       itemIndex = lastLineElement.index;
       if (beforeLastLineElement.rect.right > lastLineElement.rect.right) {
         // 最后一行还有空间
-        console.log('最后一行还有空间');
         setCanShowAll(true);
         setElementWidth(beforeLastLineElement.rect.width);
       } else {
         // 最后一行刚好用完
-        console.log('最后一行没有空间');
         setCanShowAll(false);
       };
     };
@@ -84,7 +83,6 @@ const LimitLine = () => {
    * @return {*}
    */
   const handleExpand = () => {
-    console.log('点击展开按钮', elementMap.size > maxLine);
     // 1.展示隐藏内容:重新设置 elementContainerHeight
     if (elementMap.size > maxLine) {
       // 1.1 超过四行，在四行最后一个元素添加收起按钮，并且隐藏多余元素
@@ -105,6 +103,8 @@ const LimitLine = () => {
   const handleClose = () => {
     hideMoreElement(minLine);// 隐藏两行之后的内容
     addButton(minLine, 'expand');// 添加展开按钮
+    // 移除收起按钮
+    setMaxLineLastElementIndex(null);
   };
 
   useEffect(() => {
@@ -112,12 +112,17 @@ const LimitLine = () => {
     for (let i = 0; i < children.length; i++) {
       const child = children[i];
       const rect = child.getBoundingClientRect();
-      elementMap.set(rect.bottom, { rect, index: i }); // 记录每一行最后一个元素element，元素的下标index(setMinLineLastElementIndex)。同一行bottom相同会一直覆盖；
+      if (elementMap.has(rect.top)) {
+        // @ts-ignore
+        const _lineHeight = elementMap.get(rect.top).lineHeight;
+        elementMap.set(rect.top, { rect, index: i, lineHeight: Math.max(rect.height,_lineHeight) });
+      } else {
+        elementMap.set(rect.top, { rect, index: i, lineHeight: rect.height }); // 记录每一行最后一个元素element，元素的下标index(setMinLineLastElementIndex)。同一行top相同会一直覆盖；lineHeight:行高即这一行所有元素中最大的height
+      }
       if (elementMap.size > maxLine) { // 只记录到最多展示的那一行+1(用来判断是否超过了四行)即可，因为后边的都不展示；
         break;
       };
     };
-    // console.log("useEffect", children);
     if (elementMap.size > 2) {
       hideMoreElement(minLine);// 隐藏两行之后的内容
       addButton(minLine, 'expand');// 添加展开按钮
@@ -127,12 +132,13 @@ const LimitLine = () => {
   return <>
     <QuestionLayout {...questionLayoutProps} />
     <section style={{ height: elementContainerHeight, overflow: 'hidden' }} ref={elementContainerRef} className={styles.mockElementContainer}>
-      {mockElements.map((item, i) => <section key={item} className={`${styles.mockElement} ${styles['mockElementBg' + i]}  ${(minLineLastElementIndex === i || (!canShowAll && maxLineLastElementIndex === i)) && styles.minLineLastElement}`}>
-        <>
+      {mockElements.map((item, i) => <section key={item} style={{ height: i % 2 ? '200px' : '300px' }}
+        className={`${styles.mockElement} ${styles['mockElementBg' + i]}  ${(minLineLastElementIndex === i || (!canShowAll && maxLineLastElementIndex === i)) && styles.minLineLastElement}`}>
+        <div>
           {item}
           {minLineLastElementIndex === i && <Button onClick={handleExpand} className={styles.expandButton}>展开</Button>}
           {!canShowAll && maxLineLastElementIndex === i && <Button onClick={handleClose} className={styles.expandButton}>收起</Button>}
-        </>
+        </div>
       </section>)}
       {canShowAll && <div style={{ width: elementWidth, display: 'flex', justifyContent: 'center', alignItems: 'center' }}> <Button onClick={handleClose} style={{}}>收起</Button></div>}
     </section>
